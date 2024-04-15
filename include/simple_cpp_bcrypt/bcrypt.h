3dff501cea34559fa170761a7095d0c984dd0a00
Davit Kalantaryan <davit.kalantaryan@desy.de>
2024-04-16 01:06:57 +0200

    added functions to cript the password

--- include/simple_cpp_bcrypt/bcrypt.h
+++ include/simple_cpp_bcrypt/bcrypt.h
@@ -19,6 +19,11 @@
 CPPUTILS_BEGIN_C
 
 
+SIMPCPPBCRPT_EXPORT void SimpleCppBcryptGenerateSalt(char* a_salt, size_t a_salt_size);
+SIMPCPPBCRPT_EXPORT void SimpleCppBcryptGenerateHashRaw(const char* a_salt, int a_cost, char* a_buffer_for_hash, size_t a_buffer_size);
+SIMPCPPBCRPT_EXPORT void SimpleCppBcryptGenerateHash(int a_cost, char* a_buffer_for_hash, size_t a_buffer_size);
+SIMPCPPBCRPT_EXPORT void SimpleCppBcryptNodeBcryptRaw(const char* a_key, size_t a_key_len, const char* a_hash, char* a_encrypted);
+SIMPCPPBCRPT_EXPORT void SimpleCppBcryptNodeBcrypt(const char* a_key, size_t a_key_len, int a_cost, char* a_encrypted);
 SIMPCPPBCRPT_EXPORT bool SimpleCppBcryptVerify(const char* a_password, const char* a_hash);
 SIMPCPPBCRPT_EXPORT bool SimpleCppBcryptVerifyRaw(const char* a_password, size_t a_passwordLen, const char* a_hash);
 
